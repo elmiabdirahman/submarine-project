1cd5d829dec1ed9bb1a9d85d03acefb5e3dea359
Elmi Elmi <elmielmi522@gmail.com>
2020-01-10 21:28:37 -0600

    complete with base

---
+++ server/routes/shelf.router.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const rejectUnauthenticated = require('../modules/authentication-middleware');
+
+/**
+ * Get all of the items on the shelf
+ */
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('req.user:', req.user);
+    queryText = `SELECT * FROM "secret" where "secrecy_level" <= $1;`
+    pool.query(queryText, [req.user.clearance_level])
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        });
+  });
+
+
+/**
+ * Add an item for the logged in user to the shelf
+ */
+router.post('/', (req, res) => {
+
+});
+
+
+/**
+ * Delete an item if it's something the logged in user added
+ */
+router.delete('/:id', (req, res) => {
+
+});
+
+
+/**
+ * Update an item if it's something the logged in user added
+ */
+router.put('/:id', (req, res) => {
+
+});
+
+
+/**
+ * Return all users along with the total number of items 
+ * they have added to the shelf
+ */
+router.get('/count', (req, res) => {
+
+});
+
+
+/**
+ * Return a specific item by id
+ */
+router.get('/:id', (req, res) => {
+
+});
+
+module.exports = router;
